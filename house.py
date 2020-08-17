7e752b798b1274d95a3c4a654aee5180d59428db
AlexeyWer <werneralexey@mail.ru>
2020-08-17 20:51:49 +0500

    Вывзвал функцию рисования домика

--- house.py
+++ house.py
@@ -1,6 +1,14 @@
-from graph. import *
+from graph import *
 
-def main ():
+def main():
+    windowSize(600,500)
+    paint_house(200, 200, 300, 400)
+    run()
+
+def paint_house(x, y, width, height):
+    """ Рисует домик
+        (х, у) -- опорная точка, левая точка крыши
+    """
     pass
 
 main()
